2a8591e6dbd1bb7d72f2edb394644d2c0e0873b6
FelipePrtt <felipepretti2001@gmail.com>
2025-04-02 20:36:27 -0300

    Up do novo repositório do projeto - Correção de problemas com versões entre máquinas

---
+++ projeto-ers/app/Models/Cliente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $fillable = ['nome', 'email', 'cpf', 'telefone', 'rg', 'estado', 'cidade', 'cep', 'bairro', 'endereco'];
+}
---
+++ projeto-ers/database/migrations/2025_04_02_231944_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao');
+            $table->decimal('valor_compra', 10 , 2);
+            $table->decimal('valor_venda', 10, 2);
+
+            //Campos opcionais
+            $table->unsignedBigInteger('fornecedor_id')->nullable();
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedors')->onDelete('restrict');
+            $table->date('data_compra')->nullable();
+
+            //Data de inseração de ultimo update
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
